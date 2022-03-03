f1d089afb04ce97e4221b6485cba446fc81df2f0
Graham Norris <christian.graham.norris@gmail.com>
2022-03-02 18:20:47 -0600

    code reads data from firestore + has calendar showing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./index.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Login from "./Pages/Login";
+// import Register from "./Pages/Register";
 import Home from "./Pages/Home";
-import { AuthProvider } from "./Auth";
-import PrivateRoute from "./PrivateRoute";
-import React from 'react';
+// import { AuthProvider } from "./Auth";
+// import PrivateRoute from "./PrivateRoute";
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 import DayTimePicker from '@mooncake-dev/react-day-time-picker';
 import styled from 'styled-components';
 import db from "./config"
+import {collection, getDocs} from "firebase/firestore";
 
 const Container = styled.div
   `width: 475px;
@@ -23,19 +24,91 @@ const Container = styled.div
   box-shadow: 0 2px 4px #00000018;
   @media (max-width: 520px) {
     width: 100%;
-  }`
-;
+  }`;
+  
+  function fakeRequest(data) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // Uncomment below to trigger error:
+        // return reject('Error: Something went wrong...');
+        resolve({
+          status: 'ok',
+          scheduled: data
+        });
+      }, 2e3);
+    });
+  }
+
+  function timeSlotValidator(slotTime) {
+    const startTime = new Date(
+      slotTime.getFullYear(),
+      slotTime.getMonth(),
+      slotTime.getDate(),
+      9,
+      0,
+      0
+    );
+    const endTime = new Date(
+      slotTime.getFullYear(),
+      slotTime.getMonth(),
+      slotTime.getDate(),
+      12,
+      0,
+      0
+    );
+    // Use below to see how we need user input to fill in above slots
+    // console.log("Year " +slotTime.getFullYear());
+    // console.log("Month " + slotTime.getMonth());
+    // console.log("Date " + slotTime.getDate());
+    const isValid = (slotTime.getTime() > startTime.getTime() && slotTime.getTime()<endTime.getTime()) /*&& notBooked*/;
+    return isValid;
+  }
+
+
 function App() {
+
+  const [isScheduling, setIsScheduling] = useState(false);
+  const [isScheduled, setIsScheduled] = useState(false);
+  const [scheduleErr, setScheduleErr] = useState('');
+
+  const [users, setUsers]=useState([]);
+  const usersCollectionRef = collection(db,"Users")
+
+  useEffect(() => {
+  
+    const getUsers = async () =>{
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+    };
+    getUsers();
+   
+  }, []);
+
   const handleScheduled = dateTime => {
     console.log('Scheduled booking: ', dateTime);
-    var test = db.collection('Users').collection('book')
-    test.set({
+    
+    setIsScheduling(true);
+    setScheduleErr('');
+    fakeRequest(dateTime)
+      .then(json => {
+        setScheduleErr('');
+        setIsScheduled(true);
+        console.log('fake response: ', json);
+      })
+      .catch(err => {
+        setScheduleErr(err);
+      })
+      .finally(() => {
+        setIsScheduling(false);
+        //here add to db
+      });
+    }
 
-    })
-  };
   return( 
     <Container>
-  <DayTimePicker timeSlotSizeMinutes={15} onConfirm={handleScheduled}/>
+  <DayTimePicker timeSlotSizeMinutes={15} onConfirm={handleScheduled} isLoading={isScheduling}
+      isDone={isScheduled}
+      err={scheduleErr} timeSlotValidator={timeSlotValidator}/>
   </Container>
   
   );
--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth,onAuthStateChanged } from "firebase/auth";
-import {getFirestore} from 'firebase/firestore';
+import {initializeApp } from "firebase/app";
+// import { getAuth,onAuthStateChanged } from "firebase/auth";
+import {getFirestore} from '@firebase/firestore';
 
-const firebaseApp = initializeApp({
+const firebaseApp = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
@@ -11,10 +11,11 @@ const firebaseApp = initializeApp({
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
-});
+};
 
 // Initialize Firebase
-const db = getFirestore(firebaseApp);
-const auth = getAuth(firebaseApp);
+const app = initializeApp(firebaseApp);
+const db = getFirestore(app);
+// const auth = getAuth(firebaseApp);
 
 export default db;
